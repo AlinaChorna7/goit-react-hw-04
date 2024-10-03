442af5d3139b716b4f045e894669428e0106d323
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-10-03 12:18:55 +0200

    update

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -4,7 +4,7 @@ import styles from './ImageCard.module.css';
 function ImageCard({ image, onClick }) {
   return (
     <div className={styles.ImageCard} onClick={onClick}>
-      <img src={image.urls.small} alt={image.alt_description} className={styles.image} />
+      <img src={image.urls.small} alt={image.alt_description} onClick={onClick} className={styles.image} />
     </div>
   );
 }
